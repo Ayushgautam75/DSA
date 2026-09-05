95317822097a540083c9862cb8e0dfcbd2b43dbb
Ayush <Ayushbro779@gmail.com>
2026-09-05 21:54:35 +0530

    Time: 6 ms (33.25%) | Memory: 45.3 MB (20.37%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -36,32 +36,59 @@ class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
 
         // Smaller array ko nums1 banane ke liye
-        if (nums1.length > nums2.length) {
-            return intersect(nums2, nums1);
-        }
+        // if (nums1.length > nums2.length) {
+        //     return intersect(nums2, nums1);
+        // }
 
-        Map<Integer, Integer> map = new HashMap<>();
+        // Map<Integer, Integer> map = new HashMap<>();
 
-        // Frequency of nums1
-        for (int num : nums1) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
+        // // Frequency of nums1
+        // for (int num : nums1) {
+        //     map.put(num, map.getOrDefault(num, 0) + 1);
+        // }
 
-        int[] res = new int[nums1.length];
+        // int[] res = new int[nums1.length];
+        // int k = 0;
+
+        // // Traverse nums2
+        // for (int num : nums2) {
+
+        //     int count = map.getOrDefault(num, 0);
+
+        //     if (count == 0) {
+        //         continue;
+        //     }
+
+        //     res[k++] = num;
+
+        //     map.put(num, count - 1);
+        // }
+
+        // return Arrays.copyOfRange(res, 0, k);
+
+       Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        int[] res = new int[Math.min(nums1.length, nums2.length)];
+
         int k = 0;
+        int i = 0;
+        int j = 0;
 
-        // Traverse nums2
-        for (int num : nums2) {
+        while (i < nums1.length && j < nums2.length) {
 
-            int count = map.getOrDefault(num, 0);
-
-            if (count == 0) {
-                continue;
+            if (nums1[i] == nums2[j]) {
+                res[k] = nums1[i];
+                k++;
+                i++;
+                j++;
             }
-
-            res[k++] = num;
-
-            map.put(num, count - 1);
+            else if (nums1[i] < nums2[j]) {
+                i++;
+            }
+            else {
+                j++;
+            }
         }
 
         return Arrays.copyOfRange(res, 0, k);
