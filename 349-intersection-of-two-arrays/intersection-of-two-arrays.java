eced7a4ba7debfe3974e2519b2e97e98b46eb579
Ayush <Ayushbro779@gmail.com>
2026-09-05 20:26:11 +0530

    Time: 2 ms (97.33%) | Memory: 45.1 MB (40.35%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,35 +1,66 @@
+// import java.util.*;
+
+// class Solution {
+//     public int[] intersection(int[] nums1, int[] nums2) {
+
+//         Set<Integer> s1 = new HashSet<>();
+//         Set<Integer> s2 = new HashSet<>();
+
+//         for (int num : nums1) {
+//             s1.add(num);
+//         }
+
+//         for (int num : nums2) {
+//             s2.add(num);
+//         }
+
+//         Set<Integer> result = new HashSet<>();
+
+//         for (int num : s1) {
+//             if (s2.contains(num)) {
+//                 result.add(num);
+//             }
+//         }
+
+//         int[] res = new int[result.size()];
+//         int k = 0;
+
+//         for (int num : result) {
+//             res[k] = num;
+//             k++;
+//         }
+
+//         return res;
+//     }
+// }
+
 import java.util.*;
 
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
 
-        Set<Integer> s1 = new HashSet<>();
-        Set<Integer> s2 = new HashSet<>();
+        Set<Integer> set = new HashSet<>();
 
         for (int num : nums1) {
-            s1.add(num);
-        }
-
-        for (int num : nums2) {
-            s2.add(num);
+            set.add(num);
         }
 
         Set<Integer> result = new HashSet<>();
 
-        for (int num : s1) {
-            if (s2.contains(num)) {
+        for (int num : nums2) {
+            if (set.contains(num)) {
                 result.add(num);
+                set.remove(num);   // duplicate avoid
             }
         }
 
-        int[] res = new int[result.size()];
+        int[] ans = new int[result.size()];
         int k = 0;
 
         for (int num : result) {
-            res[k] = num;
-            k++;
+            ans[k++] = num;
         }
 
-        return res;
+        return ans;
     }
 }
