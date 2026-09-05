287c507a9d2a2d4149af44405e5fe70c771da065
Ayush <Ayushbro779@gmail.com>
2026-09-05 21:40:58 +0530

    Time: 3 ms (94.85%) | Memory: 44.8 MB (77.24%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -1,17 +1,56 @@
+// class Solution {
+//     public int[] intersect(int[] nums1, int[] nums2) {
+
+//         Map<Integer, Integer> map = new HashMap<>();
+
+//         // Frequency count of nums1
+//         for (int num : nums1) {
+//             map.put(num, map.getOrDefault(num, 0) + 1);
+//         }
+
+//         int[] res = new int[Math.min(nums1.length, nums2.length)];
+//         int k = 0;
+
+//         // Check nums2
+//         for (int num : nums2) {
+
+//             int count = map.getOrDefault(num, 0);
+
+//             if (count == 0) {
+//                 continue;
+//             }
+
+//             res[k] = num;
+//             k++;
+
+//             map.put(num, count - 1);
+//         }
+
+//         return Arrays.copyOfRange(res, 0, k);
+//     }
+// }
+
+import java.util.*;
+
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
 
+        // Smaller array ko nums1 banane ke liye
+        if (nums1.length > nums2.length) {
+            return intersect(nums2, nums1);
+        }
+
         Map<Integer, Integer> map = new HashMap<>();
 
-        // Frequency count of nums1
+        // Frequency of nums1
         for (int num : nums1) {
             map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        int[] res = new int[Math.min(nums1.length, nums2.length)];
+        int[] res = new int[nums1.length];
         int k = 0;
 
-        // Check nums2
+        // Traverse nums2
         for (int num : nums2) {
 
             int count = map.getOrDefault(num, 0);
@@ -20,8 +59,7 @@ class Solution {
                 continue;
             }
 
-            res[k] = num;
-            k++;
+            res[k++] = num;
 
             map.put(num, count - 1);
         }
