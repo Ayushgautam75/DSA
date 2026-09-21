6f8894ae9bee4c2fb2797fb736e1baec671c7f2d
Ayush <Ayushbro779@gmail.com>
2026-09-21 13:58:38 +0530

    Time: 32 ms (30.21%) | Memory: 46.9 MB (72.74%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -1,15 +1,60 @@
+// class Solution {
+//     public int firstUniqChar(String s) {
+
+//         int[] freq = new int[26];
+
+       
+//         for (char ch : s.toCharArray()) {
+//             freq[ch - 'a']++;
+//         }
+
+//         for (int i = 0; i < s.length(); i++) {
+//             if (freq[s.charAt(i) - 'a'] == 1) {
+//                 return i;
+//             }
+//         }
+
+//         return -1;
+//     }
+// }
+// // class Solution {
+// //     public int firstUniqChar(String s) {
+// //         for (int i = 0; i < s.length(); i++) {
+// //             int count = 0;
+// //             for (int j = 0; j < s.length(); j++) {
+// //                 if (s.charAt(i) == s.charAt(j)) {
+// //                     count++;
+// //                 }
+// //             }
+// //             if (count == 1) {
+// //                 return i;
+// //             }
+// //         }
+// //         return -1;
+// //     }
+// // }
+
+import java.util.HashMap;
+
 class Solution {
     public int firstUniqChar(String s) {
 
-        int[] freq = new int[26];
+        HashMap<Character, Integer> map = new HashMap<>();
 
-       
-        for (char ch : s.toCharArray()) {
-            freq[ch - 'a']++;
+        // Step 1: frequency count
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
 
+        // Step 2: first character whose frequency is 1
         for (int i = 0; i < s.length(); i++) {
-            if (freq[s.charAt(i) - 'a'] == 1) {
+
+            char ch = s.charAt(i);
+
+            if (map.get(ch) == 1) {
                 return i;
             }
         }
